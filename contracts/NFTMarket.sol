a4c47a40df1b89c1fdf100cb41078dfe8ef99611
Dean Shaked <deanshak9@gmail.com>
2022-09-14 23:08:13 +0300

    added functionality to the marketplace smart contract

--- contracts/NFTMarket.sol
+++ contracts/NFTMarket.sol
@@ -12,7 +12,10 @@ contract NFTMarket is ReentrancyGuard {
     Counters.Counter private _itemSold;
 
 
+    // Marketplace owner address
     address payable owner;
+    
+    // Marketplace lising price, can also serve as commision
     uint256 listingPrice = 0.1 ether;
 
     constructor() {
@@ -29,6 +32,7 @@ contract NFTMarket is ReentrancyGuard {
         bool sold;
     }
 
+    // Map of all the items available on the market by count (1:MarketItem, 2:MarketItem, etc..)
     mapping(uint256 => MarketItem) private idToMarketItem;
     
     event MarketItemCreated (
@@ -44,11 +48,52 @@ contract NFTMarket is ReentrancyGuard {
         return listingPrice;
     }
 
-    function createMarketItem(
-        address nftContract,
-        uint256 tokenId,
-        uint price
-    ) public payable nonReentrant {
+    function createMarketItem(address nftContract, uint256 tokenId, uint price) public payable nonReentrant {
+
+        // Validate request data
+        require(price > 0,"Price must be at least 1 wei");
+        require(msg.value == listingPrice,"Price must be equal to listing price");
+
+        // Update market items indexing
+        _itemIds.increment();
+        const itemId = _itemIds.current();
+
+        // Update market items hash map, refering to address as address(0) => empty address => no owners 
+        idToMarketItem[itemId] = MarketItem(
+            itemId,
+            nftContract,
+            tokenId,
+            payable(msg.sender),
+            payable(address(0)),
+            price,
+            false
+        );
+
+        // Transfer NFT ownership to the market contract, which will eventually transfer to the next buyer
+        IERC721(nftContract).transferFrom(msg.sender,address(this),tokenId);
+
+        emit MarketItemCreated(itemId, nftContract ,tokenId ,msg.sender ,address(0) ,price ,false);
+    }
+
+    function createMarketSale(address nftContract, uint256 itemId) public payable nonReentrant {
+        uint price = idToMarketItem[itemId].price;
+        uint tokenId = idToMarketItem[itemId].tokenId;
+
+        require(msg.value == price, "Please submit the asking price in order to complete the purchase");
+        
+        // Transfer the accepted price to seller address
+        idToMarketItem[itemId].seller.transfer(msg.value);
+
+        // Transfer ownership of the NFT from the marketplace contract to the buyer
+        IERC721(nftContract).transferFrom(address(this),msg.sender,tokenId);
+
+        // Update market item properties
+        idToMarketItem[itemId].owner = payable(msg.sender);
+        idToMarketItem[itemId].sold = true;
+        _itemSold.increment();
+
+        // Pay the owner of the contract
+        payable(owner).transfer(listingPrice);
 
     }
 
