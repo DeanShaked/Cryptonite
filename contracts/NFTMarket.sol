825d39d87a5f7fb8bd464266a34aab85e189a90b
Dean Shaked <deanshak9@gmail.com>
2022-09-15 17:19:09 +0300

    added NFTMarket contract and @devnotes

--- contracts/NFTMarket.sol
+++ contracts/NFTMarket.sol
@@ -9,7 +9,7 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 contract NFTMarket is ReentrancyGuard {
     using Counters for Counters.Counter;
     Counters.Counter private _itemIds;
-    Counters.Counter private _itemSold;
+    Counters.Counter private _itemsSold;
 
 
     // Marketplace owner address
@@ -49,7 +49,6 @@ contract NFTMarket is ReentrancyGuard {
     }
 
     function createMarketItem(address nftContract, uint256 tokenId, uint price) public payable nonReentrant {
-
         // Validate request data
         require(price > 0,"Price must be at least 1 wei");
         require(msg.value == listingPrice,"Price must be equal to listing price");
@@ -58,7 +57,7 @@ contract NFTMarket is ReentrancyGuard {
         _itemIds.increment();
         uint256 itemId = _itemIds.current();
 
-        // Update market items hash map, refering to address as address(0) => empty address => no owners 
+        // Update market items hash map
         idToMarketItem[itemId] = MarketItem(
             itemId,
             nftContract,
@@ -72,11 +71,11 @@ contract NFTMarket is ReentrancyGuard {
         // Transfer NFT ownership to the market contract, which will eventually transfer to the next buyer
         IERC721(nftContract).transferFrom(msg.sender,address(this),tokenId);
 
-        // Emit events
         emit MarketItemCreated(itemId, nftContract ,tokenId ,msg.sender ,address(0));
     }
 
     function createMarketSale(address nftContract, uint256 itemId) public payable nonReentrant {
+        // Current market item properties
         uint price = idToMarketItem[itemId].price;
         uint tokenId = idToMarketItem[itemId].tokenId;
 
@@ -91,11 +90,115 @@ contract NFTMarket is ReentrancyGuard {
         // Update market item properties
         idToMarketItem[itemId].owner = payable(msg.sender);
         idToMarketItem[itemId].sold = true;
-        _itemSold.increment();
+        _itemsSold.increment();
 
-        // Pay the owner of the contract
+        // Pay comission to the marketplace owner
         payable(owner).transfer(listingPrice);
 
     }
 
+    /// @notice This function will fetch all the unsold market items
+    /// @return MarketItemArr is an array of all the unsold market items
+    
+    function fetchMarketItems() public view returns(MarketItem[] memory) {
+
+        // Counter of total items created on market
+        uint itemCount = _itemIds.current();
+
+        // Counter of total unsold items on market
+        uint unsoldItemsCount = _itemIds.current() - _itemsSold.current();
+
+        // unsold items index
+        uint currentIndex = 0;
+
+        // Total unsold market items array
+        MarketItem[] memory unsoldItems = new MarketItem[](unsoldItemsCount);
+
+        // Loop over the items that has been created,
+        // Check if the item is unsold
+        // Create a market item reference
+        // Add the market item reference to the items arr
+        for(uint i = 0; i < itemCount ;i++) {
+            if(idToMarketItem[i+1].owner == address(0)) {
+
+                uint currentId = idToMarketItem[i+1].itemId;
+                MarketItem storage currentItem = idToMarketItem[currentId];
+                unsoldItems[currentIndex] = currentItem;
+
+                currentIndex += 1;
+            }
+        }
+        
+        return unsoldItems;
+    }
+
+    /// @notice This function will fetch the NFTs that the user purchased
+    /// @return purchasedItemsArr is an array of all the NFTs the user has purchased
+    
+    function fetchItemsPurchased() public view returns(MarketItem[] memory) {
+
+        uint totalItemCount = _itemIds.current();
+        uint itemCount = 0;
+        uint currentIndex = 0;
+
+        // Find the length of array of items that was purchased by the user
+        for(uint i = 0; i < totalItemCount; i++) {
+            if(idToMarketItem[i + 1].owner == msg.sender) {
+                itemCount += 1;
+            }
+        }
+
+        MarketItem[] memory purchasedItemsArr = new MarketItem[](itemCount);
+
+        for(uint i = 0; i < itemCount; i++) {
+            if(idToMarketItem[i + 1].owner == msg.sender) {
+                uint currentId = idToMarketItem[i + 1].itemId;
+                MarketItem storage currentItem = idToMarketItem[currentId];
+                purchasedItemsArr[currentIndex] = currentItem;
+                currentIndex += 1;
+            }
+        }
+        return purchasedItemsArr;
+    }
+
+    /// @notice This function will fetch the NFTs that the user created
+    /// @return createdItemsArr is an array of all the NFTs the user has created 
+    
+    function fetchItemsCreated() public view returns(MarketItem[] memory) {
+
+        uint totalItemCount = _itemIds.current();
+        uint itemCount = 0;
+        uint currentIndex = 0;
+
+        // Find the length of array of items that was created by the user
+        for(uint i = 0; i < totalItemCount; i++) {
+            if(idToMarketItem[i + 1].seller == msg.sender) {
+                itemCount += 1;
+            }
+        }
+
+        MarketItem[] memory createdItemsArr = new MarketItem[](itemCount);
+
+        // Find the current item and add him to createdItemsArr
+        for(uint i = 0; i < itemCount; i++) {
+            if(idToMarketItem[i + 1].seller == msg.sender) {
+                uint currentId = idToMarketItem[i + 1].itemId;
+                MarketItem storage currentItem = idToMarketItem[currentId];
+                createdItemsArr[currentIndex] = currentItem;
+                currentIndex += 1;
+            }
+        }
+        return createdItemsArr;
+    }
+
 }
+
+/// @dev notes:
+/// - address(0) is an empty address, which in our case means no owners
+/// - address(this) is marketplace smart contract address
+/// - "MarketItem[] memory" is an array that will reset once the function execution ends
+/// - "MarketItem storage" is an object that once you change it it'll update the contract state
+/// - "public view" is mostly used for fetching data functions
+/// - "public payable" is mostly used for payments transfer
+/// - "nonReentrant" is the ReentrancyGuard modifier that controlls the function entrancy state
+/// - 
