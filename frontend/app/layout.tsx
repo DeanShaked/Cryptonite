ca00faad4cb16f21354940bad0f7c90b9a7b0d45
Dean Shaked <deanshak9@gmail.com>
2022-09-18 11:29:00 +0300

    dsdasd

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -5,8 +5,7 @@ import React from 'react';
 import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
 
-const RootLayout = (props: any) => {
-  const { children } = props;
+const RootLayout = () => {
   return (
     <>
       <Header />
